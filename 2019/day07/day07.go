b1d53f08baff3de39ceb72d2058c1e3d91ff2d6a
Harry Poulter <hdp1213@hotmail.com>
2019-12-08 04:03:40 +1030

    Solve day 7 part one
    I'm in love with my intcode computer.

--- 2019/day07/day07.go
+++ 2019/day07/day07.go
@@ -73,8 +73,34 @@ func RunAmplification(code, phaseSettings []int) (int, error) {
 	return input, nil
 }
 
+// from https://stackoverflow.com/questions/30226438/generate-all-permutations-in-go
 func permuteArray(array []int) [][]int {
-	return [][]int { array }
+	var helper func([]int, int)
+    res := [][]int{}
+
+    helper = func(arr []int, n int){
+        if n == 1{
+            tmp := make([]int, len(arr))
+            copy(tmp, arr)
+            res = append(res, tmp)
+        } else {
+            for i := 0; i < n; i++{
+                helper(arr, n - 1)
+                if n % 2 == 1{
+                    tmp := arr[i]
+                    arr[i] = arr[n - 1]
+                    arr[n - 1] = tmp
+                } else {
+                    tmp := arr[0]
+                    arr[0] = arr[n - 1]
+                    arr[n - 1] = tmp
+                }
+            }
+        }
+    }
+
+    helper(array, len(array))
+    return res
 }
 
 // Solve solves both parts of the problem
