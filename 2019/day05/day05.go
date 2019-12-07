2956cdba731ff6fb5b2caef6e78f317d5d34516b
Harry Poulter <hdp1213@hotmail.com>
2019-12-08 01:17:17 +1030

    Solve day 5 part two
    With the aid of many lovely unit tests, goodness how lovely they all
    are! Wow, I absolutely loved doing this day's problems, gosh. How
    stunning.

--- 2019/day05/day05.go
+++ 2019/day05/day05.go
@@ -50,7 +50,6 @@ func createParameters(values, modes []int) ([]Parameter, error) {
 type Instruction struct {
 	opCode, numValues int
 	op func(*IntcodeComputer, int, []Parameter) error
-	doesJump bool
 }
 
 // Execute runs an instruction's operation on the given computer
@@ -100,27 +99,53 @@ func instructionInput(computer *IntcodeComputer, input int, parameters []Paramet
 
 func instructionOutput(computer *IntcodeComputer, input int, parameters []Parameter) error {
 	output := parameters[0].getValue(computer.memory)
-	fmt.Println(output)
+	computer.output = append(computer.output, output)
 	return nil
 }
 
 func instructionJumpIfTrue(computer *IntcodeComputer, input int, parameters []Parameter) error {
 	if parameters[0].getValue(computer.memory) != 0 {
-		// set instruction pointer to value of second parameter
+		computer.instructionPtr = parameters[1].getValue(computer.memory)
+		computer.hasIncremented = true
 	}
 
 	return nil
 }
 
 func instructionJumpIfFalse(computer *IntcodeComputer, input int, parameters []Parameter) error {
+	if parameters[0].getValue(computer.memory) == 0 {
+		computer.instructionPtr = parameters[1].getValue(computer.memory)
+		computer.hasIncremented = true
+	}
+
 	return nil
 }
 
 func instructionLessThan(computer *IntcodeComputer, input int, parameters []Parameter) error {
+	if parameters[2].mode == immediateMode {
+		return errors.New("write parameter cannot be in immediate mode")
+	}
+
+	if parameters[0].getValue(computer.memory) < parameters[1].getValue(computer.memory) {
+		computer.memory[parameters[2].value] = 1
+	} else {
+		computer.memory[parameters[2].value] = 0
+	}
+
 	return nil
 }
 
 func instructionEquals(computer *IntcodeComputer, input int, parameters []Parameter) error {
+	if parameters[2].mode == immediateMode {
+		return errors.New("write parameter cannot be in immediate mode")
+	}
+
+	if parameters[0].getValue(computer.memory) == parameters[1].getValue(computer.memory) {
+		computer.memory[parameters[2].value] = 1
+	} else {
+		computer.memory[parameters[2].value] = 0
+	}
+
 	return nil
 }
 
@@ -133,7 +158,6 @@ var Add = Instruction {
 	opCode: 1,
 	op: instructionAdd,
 	numValues: 4,
-	doesJump: false,
 }
 
 // Multiply is an instruction that multiplies two elements together
@@ -141,7 +165,6 @@ var Multiply = Instruction {
 	opCode: 2,
 	op: instructionMultiply,
 	numValues: 4,
-	doesJump: false,
 }
 
 // Input is an instruction that takes input and saves it to a memory location
@@ -149,7 +172,6 @@ var Input = Instruction {
 	opCode: 3,
 	op: instructionInput,
 	numValues: 2,
-	doesJump: false,
 }
 
 // Output is an instruction that writes output from given memory location
@@ -157,7 +179,34 @@ var Output = Instruction {
 	opCode: 4,
 	op: instructionOutput,
 	numValues: 2,
-	doesJump: false,
+}
+
+// JumpIfTrue is an instruction that jumps execution if the first parameter is zero
+var JumpIfTrue = Instruction {
+	opCode: 5,
+	op: instructionJumpIfTrue,
+	numValues: 3,
+}
+
+// JumpIfFalse is an instruction that jumps execution if the first parameter is non-zero
+var JumpIfFalse = Instruction {
+	opCode: 6,
+	op: instructionJumpIfFalse,
+	numValues: 3,
+}
+
+// LessThan is an instruction that stores 1 if first < second, else 0
+var LessThan = Instruction {
+	opCode: 7,
+	op: instructionLessThan,
+	numValues: 4,
+}
+
+// Equals is an instruction that stores 1 if first == second, else 0
+var Equals = Instruction {
+	opCode: 8,
+	op: instructionEquals,
+	numValues: 4,
 }
 
 // End is an instruction that ends the program
@@ -165,7 +214,6 @@ var End = Instruction {
 	opCode: EndCode,
 	op: instructionEnd,
 	numValues: 1,
-	doesJump: false,
 }
 
 // IntcodeComputer runs an intcode program
@@ -174,6 +222,7 @@ type IntcodeComputer struct {
 	instructionPtr int
 	instructions map[int]*Instruction
 	output []int
+	hasIncremented bool
 }
 
 // AddInstruction adds an Instruction to an IntcodeComputer
@@ -181,8 +230,8 @@ func (computer *IntcodeComputer) AddInstruction(in *Instruction) {
 	computer.instructions[in.opCode] = in
 }
 
-func (computer *IntcodeComputer) parseParameters(numValues int, doIncrement bool) []int {
-	if doIncrement {
+func (computer *IntcodeComputer) parseParameters(numValues int) []int {
+	if !computer.hasIncremented {
 		defer computer.incrementBy(numValues)
 	}
 	return computer.memory[computer.instructionPtr + 1:computer.instructionPtr + numValues]
@@ -201,9 +250,10 @@ func (computer *IntcodeComputer) copyMemory() {
 func (computer *IntcodeComputer) parseNextInstruction() (*Instruction, []Parameter, error) {
 	fullCode := computer.memory[computer.instructionPtr]
 	modeCode, opCode := splitCode(fullCode)
+	computer.hasIncremented = false
 
 	if instruction, ok := computer.instructions[opCode]; ok {
-		values := computer.parseParameters(instruction.numValues, !instruction.doesJump)
+		values := computer.parseParameters(instruction.numValues)
 		modes, err := parseModes(opCode, modeCode, instruction.numValues)
 		if err != nil {
 			return instruction, []Parameter{}, err
@@ -252,6 +302,7 @@ func parseModes(opCode, modeCode, numValues int) ([]int, error) {
 // Run runs the IntcodeComputer's program
 func (computer *IntcodeComputer) Run(input int) error {
 	computer.copyMemory()
+	computer.output = []int{}
 	computer.instructionPtr = 0
 
 	instruction, parameters, err := computer.parseNextInstruction()
@@ -264,15 +315,15 @@ func (computer *IntcodeComputer) Run(input int) error {
 			return nil
 		}
 
+		if computer.instructionPtr >= len(computer.memory) {
+			return errors.New("reached end of memory without end opcode")
+		}
+
 		instruction.Execute(computer, input, parameters...)
 
 		instruction, parameters, err = computer.parseNextInstruction()
 		if err != nil {
 			return err
-		}
-
-		if computer.instructionPtr >= len(computer.memory) {
-			return errors.New("reached end of memory without end opcode")
 		}
 	}
 }
@@ -284,6 +335,7 @@ func NewComputer(initMemory []int, instructions ...*Instruction) (IntcodeCompute
 		memory: make([]int, len(initMemory)),
 		instructionPtr: 0,
 		output: []int{},
+		hasIncremented: false,
 	}
 
 	computer.instructions = make(map[int]*Instruction, len(instructions))
@@ -329,19 +381,39 @@ func Solve() {
 		return
 	}
 
-	computer, err := NewComputer(initMemory, &Add, &Multiply, &Input, &Output, &End)
+	computer1, err := NewComputer(initMemory, &Add, &Multiply, &Input, &Output, &End)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "computer failed to initialise")
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 
-	err = computer.Run(1)
+	err = computer1.Run(1)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		fmt.Fprintf(os.Stderr, "computer failed to run, reached instruction %d\n", computer.instructionPtr)
-		// fmt.Fprintln(os.Stderr, "memory dump:")
-		// PrintIntcode(computer.memory)
+		fmt.Fprintf(os.Stderr, "computer failed to run, reached instruction %d\n", computer1.instructionPtr)
+		fmt.Fprintln(os.Stderr, "memory dump:")
+		PrintIntcode(computer1.memory)
 		return 
 	}
+
+	fmt.Printf("first computer output = %v\n", computer1.output)
+
+	computer2, err := NewComputer(initMemory, &Add, &Multiply, &Input, &Output, &JumpIfTrue, &JumpIfFalse, &LessThan, &Equals, &End)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "computer failed to initialise")
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	err = computer2.Run(5)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintf(os.Stderr, "computer failed to run, reached instruction %d\n", computer2.instructionPtr)
+		fmt.Fprintln(os.Stderr, "memory dump:")
+		PrintIntcode(computer2.memory)
+		return 
+	}
+
+	fmt.Printf("second computer output = %v\n", computer2.output)
 }
--- 2019/day05/day05_test.go
+++ 2019/day05/day05_test.go
@@ -3,7 +3,9 @@ package day05
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"testing"
+	"github.com/hdp1213/AdventOfCode/2019/day02"
 )
 
 func compareArrays(expected, actual []int, arrayName string) error {
@@ -19,6 +21,11 @@ func compareArrays(expected, actual []int, arrayName string) error {
 	}
 
 	return nil
+}
+
+func readIntcodeString(intcode string) ([]int, error) {
+	reader := strings.NewReader(intcode)
+	return day02.ReadIntcode(reader)
 }
 
 // TestParseModesSymmetric tests the parseModes function in success
@@ -105,5 +112,184 @@ func TestCreateParametersBad(t *testing.T) {
 	_, err := createParameters(values, modes)
 	if err == nil {
 		t.Error("expected error")
+	}
+}
+
+
+type intcodeTest struct {
+	intcode string
+	inputs, outputs []int
+}
+
+
+// TestComputerEqual tests equals functionality
+func TestComputerEqual(t *testing.T) {
+	inputs := []int {7, 8, 9}
+	outputs := []int {0, 1, 0}
+
+	intcodeTests := []intcodeTest {
+		intcodeTest {
+			intcode: "3,9,8,9,10,9,4,9,99,-1,8",
+			inputs: inputs,
+			outputs: outputs,
+		},
+		intcodeTest {
+			intcode: "3,3,1108,-1,8,3,4,3,99",
+			inputs: inputs,
+			outputs: outputs,
+		},
+	}
+
+	for _, test := range intcodeTests {
+		initMemory, err := readIntcodeString(test.intcode)
+		if err != nil {
+			t.Error(err)
+		}
+
+		computer, err := NewComputer(initMemory, &Input, &Output, &Equals, &End)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for i, input := range test.inputs {
+			err = computer.Run(input)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(computer.output) != 1 {
+				t.Errorf("expected len(computer.output) == 1, got %d", len(computer.output))
+			}
+
+			if computer.output[0] != test.outputs[i] {
+				t.Errorf("expected output = %d, got %d", test.outputs[i], computer.output[0])
+			}
+		}
+	}
+}
+
+// TestComputerLessThan tests less than functionality
+func TestComputerLessThan(t *testing.T) {
+	inputs := []int {7, 8, 9}
+	outputs := []int {1, 0, 0}
+
+	intcodeTests := []intcodeTest {
+		intcodeTest {
+			intcode: "3,9,7,9,10,9,4,9,99,-1,8",
+			inputs: inputs,
+			outputs: outputs,
+		},
+		intcodeTest {
+			intcode: "3,3,1107,-1,8,3,4,3,99",
+			inputs: inputs,
+			outputs: outputs,
+		},
+	}
+
+	for _, test := range intcodeTests {
+		initMemory, err := readIntcodeString(test.intcode)
+		if err != nil {
+			t.Error(err)
+		}
+
+		computer, err := NewComputer(initMemory, &Input, &Output, &LessThan, &End)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for i, input := range test.inputs {
+			err =computer.Run(input)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(computer.output) != 1 {
+				t.Errorf("expected len(computer.output) == 1, got %d", len(computer.output))
+			}
+
+			if computer.output[0] != test.outputs[i] {
+				t.Errorf("expected output = %d, got %d", test.outputs[i], computer.output[0])
+			}
+		}
+	}
+}
+
+// TestComputerJump tests jump functionality
+func TestComputerJump(t *testing.T) {
+	inputs := []int {0, 1, -1}
+	outputs := []int {0, 1, 1}
+
+	intcodeTests := []intcodeTest {
+		intcodeTest {
+			intcode: "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9",
+			inputs: inputs,
+			outputs: outputs,
+		},
+		intcodeTest {
+			intcode: "3,3,1105,-1,9,1101,0,0,12,4,12,99,1",
+			inputs: inputs,
+			outputs: outputs,
+		},
+	}
+
+	for _, test := range intcodeTests {
+		initMemory, err := readIntcodeString(test.intcode)
+		if err != nil {
+			t.Error(err)
+		}
+
+		computer, err := NewComputer(initMemory, &Add, &Input, &Output, &JumpIfTrue, &JumpIfFalse, &End)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for i, input := range test.inputs {
+			err = computer.Run(input)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(computer.output) != 1 {
+				t.Errorf("expected len(computer.output) == 1, got %d", len(computer.output))
+			}
+
+			if computer.output[0] != test.outputs[i] {
+				t.Errorf("expected output = %d, got %d", test.outputs[i], computer.output[0])
+			}
+		}
+	}
+}
+
+// TestComputerLargerExample tests the larger part 2 example
+func TestComputerLargerExample(t *testing.T) {
+	test := intcodeTest {
+		intcode: "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+		inputs: []int {-100, 7, 8, 9, 100},
+		outputs: []int {999, 999, 1000, 1001, 1001},
+	}
+
+	initMemory, err := readIntcodeString(test.intcode)
+	if err != nil {
+		t.Error(err)
+	}
+
+	computer, err := NewComputer(initMemory, &Add, &Multiply, &Input, &Output, &JumpIfTrue, &JumpIfFalse, &LessThan, &Equals, &End)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, input := range test.inputs {
+		err = computer.Run(input)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if len(computer.output) != 1 {
+			t.Errorf("expected len(computer.output) == 1, got %d", len(computer.output))
+		}
+
+		if computer.output[0] != test.outputs[i] {
+			t.Errorf("expected output = %d, got %d", test.outputs[i], computer.output[0])
+		}
 	}
 }
